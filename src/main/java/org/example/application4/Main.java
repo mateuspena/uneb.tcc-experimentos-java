bd69a91545bff827b987c87decb6cbcc3e22dcb5
Mateus Pena <mateus.18j@gmail.com>
2022-10-18 14:20:23 -0300

    App 4.

--- src/main/java/org/example/application4/Main.java
+++ src/main/java/org/example/application4/Main.java
@@ -6,7 +6,6 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
 
 public class Main {
   public static void main(String[] args) throws IOException {
